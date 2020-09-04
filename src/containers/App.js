8f7ab1a0ffc4f7a6369f884e7a4e68dc5b8342ac
XimenaCor <cor.xime@gmail.com>
2020-09-04 11:24:09 -0400

    feat: add header compnent

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import Header from '../components/Header';
 import ErrorBoundry from '../components/ErrorBoundry';
 
 import { setSearchField, requestRobots } from '../actions';
@@ -42,7 +43,7 @@ class App extends Component {
       <h1>Loading</h1> :
       (
         <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
+          <Header />
           <SearchBox searchChange={onSearchChange}/>
           <Scroll>
             <ErrorBoundry>
