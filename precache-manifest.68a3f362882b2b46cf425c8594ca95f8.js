16e49b76debdb777f51747e8afb6543dda39fd23
XimenaCor <cor.xime@gmail.com>
2020-09-04 13:05:05 -0400

    Updates

---
+++ precache-manifest.68a3f362882b2b46cf425c8594ca95f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5549bd4b8cac9ddc9adff3f441aa87f0",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "31765020f44480377980",
+    "url": "/monsters-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9f689ca1f2b1f1283c54",
+    "url": "/monsters-app/static/css/main.63a74c7e.chunk.css"
+  },
+  {
+    "revision": "31765020f44480377980",
+    "url": "/monsters-app/static/js/2.7360ae8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/monsters-app/static/js/2.7360ae8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f689ca1f2b1f1283c54",
+    "url": "/monsters-app/static/js/main.7d023933.chunk.js"
+  },
+  {
+    "revision": "7138d42281881b672924",
+    "url": "/monsters-app/static/js/runtime-main.ce03e1fd.js"
+  }
+]);
